913284780d03681b7a813578d66546f59c26e4d5
iamvivekchadhuvula <iamvivekchadhuvula@gmail.com>
2024-06-01 14:04:26 +0530

    connection is established

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+const express = require('express');
+const {config} = require('dotenv');
 const http = require('http');
-const { config } = require('dotenv')
 
+// const io = require('socket.io')
+const { Server } = require('socket.io')
 config();
 
-const server = http.createServer();
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-server.listen( process.env.PORT || 3000 ,() =>{
-  console.log(`server is running on port ${process.env.PORT}`)
-});
+let listener = io.listen(server)
+listener.socket.on('connection', () => {
+  socket.emit('messages', {
+    'message':'hello '
+  })
+})
+io.on('connection', (socket) =>{
+  console.log('a user is connected')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port:${process.env.PORT}`)
+})
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/socket', (req,res) => {
+  res.sendFile(__dirname + '/public/socket.html')
+})
